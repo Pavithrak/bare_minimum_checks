78ea27168a78e55cba4fa7f9c9f348efa455b2fa
Pavithra Krishnan <pavikrish2988@gmail.com>
2019-08-01 14:01:15 -0600

    Read from config file

--- lib/bare_minimum_checks.rb
+++ lib/bare_minimum_checks.rb
@@ -1,13 +1,15 @@
 require_relative "bare_minimum_checks/version"
 require_relative 'bare_minimum_checks/project'
+require 'yaml'
 
 module BareMinimumChecks
     class SafePush
       def run_specs
-        project = BareMinimumChecks::Project.new
-        project.run_specs
+        config = YAML.load(File.open("sample_jobber.yml"))
+        puts config
+        project = BareMinimumChecks::Project.new(config)
+        puts project.get_test_files
+        # project.run_specs
       end
     end
 end
-
-
--- lib/bare_minimum_checks/project.rb
+++ lib/bare_minimum_checks/project.rb
@@ -3,6 +3,32 @@ require_relative 'file_name_style'
 
 module BareMinimumChecks
   class Project
+    def initialize(config)
+      @config = config
+    end
+
+    def get_test_files
+      find_files = @config["tests"]["find"]
+      local_changes.reduce([]) do |specs, full_file_name|
+        file_path = File.dirname(full_file_name)
+        file_path.gsub!(find_files["strip"], ".")
+        file_name = File.basename(full_file_name, ".*")
+        paths = find_files["paths"]
+        test_files = paths.map do |path|
+          test_file = path.gsub("{path_to_file}", file_path)
+          .gsub("{file_name}", file_name)
+          if File.file?(test_file)
+            test_file
+          else
+            puts "File #{test_file} not found. Unable to test changes in #{full_file_name}"
+            nil
+          end
+        end.compact
+        specs.push(*test_files)
+        specs
+      end
+    end
+
     def run_specs
       specs = local_changes.reduce([]) do |specs, file_path|
         arr = specs.concat FileNameStyle.camel_case.get_test_file_name(file_path)
